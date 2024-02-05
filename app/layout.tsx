01e398e9e387e59275f5d0f560efc513f564027b
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2024-02-05 21:32:16 +0300

    og image fix v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,21 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     site: "@nahusenayhotel",
-    }
+    },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://web.nahusenayinternationalhotel.com/",
+    siteName: "Nahusenay International Hotel",
+    images: [
+      {
+        url: "/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Nahusenay International Hotel",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
